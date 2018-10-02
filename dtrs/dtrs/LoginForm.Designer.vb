cce657f4095bd6b863dfe242883cb8c3f62de5fe
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2018-10-02 12:59:46 +0800

    Base Login Form

---
+++ dtrs/dtrs/LoginForm.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LoginForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.labelUsername = New System.Windows.Forms.Label()
+        Me.labelPassword = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'labelUsername
+        '
+        Me.labelUsername.AutoSize = True
+        Me.labelUsername.Location = New System.Drawing.Point(201, 91)
+        Me.labelUsername.Name = "labelUsername"
+        Me.labelUsername.Size = New System.Drawing.Size(55, 13)
+        Me.labelUsername.TabIndex = 0
+        Me.labelUsername.Text = "Username"
+        '
+        'labelPassword
+        '
+        Me.labelPassword.AutoSize = True
+        Me.labelPassword.Location = New System.Drawing.Point(201, 144)
+        Me.labelPassword.Name = "labelPassword"
+        Me.labelPassword.Size = New System.Drawing.Size(53, 13)
+        Me.labelPassword.TabIndex = 1
+        Me.labelPassword.Text = "Password"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(305, 91)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(305, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(305, 144)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(305, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(305, 208)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = False
+        '
+        'LoginForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.labelPassword)
+        Me.Controls.Add(Me.labelUsername)
+        Me.Name = "LoginForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "DTR System"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents labelUsername As Label
+    Friend WithEvents labelPassword As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents btnLogin As Button
+End Class
---
+++ dtrs/dtrs/LoginForm.vb
@@ -0,0 +1,5 @@
+﻿Public Class LoginForm
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+
+    End Sub
+End Class
