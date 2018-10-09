ee81fa98c3b9907ad89801962612025bef3e63aa
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2018-10-09 23:23:12 +0800

    Return menu strip functionality

--- dtrs/dtrs/MainAdminFormEmployees.vb
+++ dtrs/dtrs/MainAdminFormEmployees.vb
@@ -33,4 +33,17 @@
 
         DTRForm.Show()
     End Sub
+
+    Private Sub EmployeesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EmployeesToolStripMenuItem.Click
+        Me.Hide()
+        Me.Show()
+    End Sub
+    Private Sub PositionsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PositionsToolStripMenuItem.Click
+        Me.Hide()
+        MainAdminFormPositions.Show()
+    End Sub
+    Private Sub AdminsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AdminsToolStripMenuItem.Click
+        Me.Hide()
+        MainAdminFormAdmins.Show()
+    End Sub
 End Class
